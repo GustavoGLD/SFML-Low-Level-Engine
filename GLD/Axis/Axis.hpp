d3df89090e66ce4e4744cbaf72cc5e3f2bca8979
!Gustavo! <72351688+GustavoGLD@users.noreply.github.com>
2021-08-15 23:23:48 -0300

    Update Axis.hpp

--- GLD/Axis/Axis.hpp
+++ GLD/Axis/Axis.hpp
@@ -9,7 +9,17 @@ namespace GLD
 ////////////////////////////////////////////////////////////////////////////////////////////
 /// \brief Getting input from the W-S, D-A and Q-E Axis. Return a value between -1 and 1
 ////////////////////////////////////////////////////////////////////////////////////////////
-#include "AxisImpl.cpp"
+  
+struct _Axle {
+    sf::Keyboard::Key positive;
+    sf::Keyboard::Key negative;
+};
+std::vector<_Axle> _axis_array = {
+    //positive             //negative
+    {sf::Keyboard::Key::D, sf::Keyboard::Key::A}, //horizontal
+    {sf::Keyboard::Key::S, sf::Keyboard::Key::W}, //vertical
+    {sf::Keyboard::Key::E, sf::Keyboard::Key::Q}  //diagonal
+};
 
 ////////////////////////////////////////////////////////////
 /// \brief the possible Axis to use
